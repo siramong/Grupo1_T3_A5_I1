684f22dc079ea2655031ca5a8e5e2c978d1bfd9a
SirAmong <ariel.morocho@hotmail.com>
2025-03-25 20:44:28 -0500

    Finished Working View Button Lists

--- src/proyecto/Controlador.java
+++ src/proyecto/Controlador.java
@@ -9,21 +9,52 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import proyecto.vistas.AcercaDe;
 import proyecto.vistas.MenuPrincipal;
+import proyecto.vistas.CrearVector;
+import proyecto.vistas.IngresarDatos;
+import proyecto.vistas.Instructivo;
+import proyecto.vistas.MostrarListado;
+import proyecto.vistas.MostrarMayor;
 
 public class Controlador implements ActionListener {
     
     MenuPrincipal vistaPrincipal;
     Modelo modeloMain;
     AcercaDe vistaAcercaDe;
+    CrearVector vistaCrearVector;
+    IngresarDatos vistaIngresarDatos;
+    Instructivo vistaInstructivo;
+    MostrarListado vistaMostrarListado;
+    MostrarMayor vistaMostrarMayor;
     
     public Controlador(MenuPrincipal vistaPrincipal, Modelo modeloMain) {
         this.vistaPrincipal = vistaPrincipal;
         this.modeloMain = modeloMain;
         
-        //BLOQUE DE EJECUCION PARA VISTAS
+        this.vistaPrincipal.btnSalir.addActionListener(this);
+        
         this.vistaAcercaDe = new AcercaDe();
         vistaPrincipal.btnAcercaDe.addActionListener(this);
         vistaAcercaDe.btnCerrarAcercaDe.addActionListener(this);
+        
+        this.vistaCrearVector = new CrearVector();
+        vistaPrincipal.btnCrearVector.addActionListener(this);
+        vistaCrearVector.btnCerrarCrearVector.addActionListener(this);
+        
+        this.vistaIngresarDatos = new IngresarDatos();
+        vistaPrincipal.btnIngresarDatos.addActionListener(this);
+        vistaIngresarDatos.btnCerrarIngresarDatos.addActionListener(this);
+        
+        this.vistaInstructivo = new Instructivo();
+        vistaPrincipal.btnInstructivo.addActionListener(this);
+        vistaInstructivo.btnCerrarInstructivo.addActionListener(this);
+        
+        this.vistaMostrarListado = new MostrarListado();
+        vistaPrincipal.btnMostrarListado.addActionListener(this);
+        vistaMostrarListado.btnCerrarMostrarListado.addActionListener(this);
+        
+        this.vistaMostrarMayor = new MostrarMayor();
+        vistaPrincipal.btnMostrarMayor.addActionListener(this);
+        vistaMostrarMayor.btnCerrarMostrarMayor.addActionListener(this);
     }
     
     @Override
@@ -31,8 +62,30 @@ public class Controlador implements ActionListener {
         Object buttonPressed = ae.getSource();
         if (buttonPressed == vistaPrincipal.btnAcercaDe) {
             vistaAcercaDe.setVisible(true);
-        } else if (buttonPressed == vistaAcercaDe.btnCerrarAcercaDe){
+        } else if (buttonPressed == vistaPrincipal.btnCrearVector) {
+            vistaCrearVector.setVisible(true);
+        } else if (buttonPressed == vistaPrincipal.btnIngresarDatos) {
+            vistaIngresarDatos.setVisible(true);
+        } else if (buttonPressed == vistaPrincipal.btnInstructivo) {
+            vistaInstructivo.setVisible(true);
+        } else if (buttonPressed == vistaPrincipal.btnMostrarListado) {
+            vistaMostrarListado.setVisible(true);
+        } else if (buttonPressed == vistaPrincipal.btnMostrarMayor) {
+            vistaMostrarMayor.setVisible(true);
+        } else if (buttonPressed == vistaAcercaDe.btnCerrarAcercaDe) {
             vistaAcercaDe.setVisible(false);
+        } else if (buttonPressed == vistaCrearVector.btnCerrarCrearVector) {
+            vistaCrearVector.setVisible(false);
+        } else if (buttonPressed == vistaIngresarDatos.btnCerrarIngresarDatos) {
+            vistaIngresarDatos.setVisible(false);
+        } else if (buttonPressed == vistaInstructivo.btnCerrarInstructivo) {
+            vistaInstructivo.setVisible(false);
+        } else if (buttonPressed == vistaMostrarListado.btnCerrarMostrarListado) {
+            vistaMostrarListado.setVisible(false);
+        } else if (buttonPressed == vistaMostrarMayor.btnCerrarMostrarMayor) {
+            vistaMostrarMayor.setVisible(false);
+        } else if(buttonPressed == vistaPrincipal.btnSalir) {
+            vistaPrincipal.setVisible(false);
         }
     }
 }
