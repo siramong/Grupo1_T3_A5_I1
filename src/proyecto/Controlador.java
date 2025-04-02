d3baeeccc1ecab9e046d8359844718c720052bb9
flordefuegoyseta <oliver.montalvanj.est@uets.edu.ec>
2025-04-01 21:56:32 -0500

    documentacion

--- src/proyecto/Controlador.java
+++ src/proyecto/Controlador.java
@@ -5,6 +5,7 @@
  */
 package proyecto;
 
+//importamos los action event y listener, ademas de todas las vistas
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.DefaultListModel;
@@ -17,7 +18,7 @@ import proyecto.vistas.MostrarListado;
 import proyecto.vistas.MostrarMayor;
 
 public class Controlador implements ActionListener {
-
+    //definimos cada vista con su propia variable
     MenuPrincipal vistaPrincipal;
     Modelo modeloMain;
     AcercaDe vistaAcercaDe;
@@ -32,29 +33,29 @@ public class Controlador implements ActionListener {
         this.modeloMain = modeloMain;
 
         this.vistaPrincipal.btnSalir.addActionListener(this);
-
+        //hacer que los botones puedan ser "escuchados" o que manden una señal al ser precionados
         this.vistaAcercaDe = new AcercaDe();
         vistaPrincipal.btnAcercaDe.addActionListener(this);
         vistaAcercaDe.btnCerrarAcercaDe.addActionListener(this);
-
+        //hacer que los botones puedan ser "escuchados" o que manden una señal al ser precionados
         this.vistaCrearVector = new CrearVector();
         vistaPrincipal.btnCrearVector.addActionListener(this);
         vistaCrearVector.btnCerrarCrearVector.addActionListener(this);
         vistaCrearVector.btnEnviarVector.addActionListener(this);
-
+        //hacer que los botones puedan ser "escuchados" o que manden una señal al ser precionados
         this.vistaIngresarDatos = new IngresarDatos();
         vistaPrincipal.btnIngresarDatos.addActionListener(this);
         vistaIngresarDatos.btnCerrarIngresarDatos.addActionListener(this);
         vistaIngresarDatos.btnEnviarDatosVector.addActionListener(this);
-
+        //hacer que los botones puedan ser "escuchados" o que manden una señal al ser precionados
         this.vistaInstructivo = new Instructivo();
         vistaPrincipal.btnInstructivo.addActionListener(this);
         vistaInstructivo.btnCerrarInstructivo.addActionListener(this);
-
+        //hacer que los botones puedan ser "escuchados" o que manden una señal al ser precionados
         this.vistaMostrarListado = new MostrarListado();
         vistaPrincipal.btnMostrarListado.addActionListener(this);
         vistaMostrarListado.btnCerrarMostrarListado.addActionListener(this);
-
+        //hacer que los botones puedan ser "escuchados" o que manden una señal al ser precionados
         this.vistaMostrarMayor = new MostrarMayor();
         vistaPrincipal.btnMostrarMayor.addActionListener(this);
         vistaMostrarMayor.btnCerrarMostrarMayor.addActionListener(this);
@@ -63,6 +64,7 @@ public class Controlador implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent ae) {
         Object buttonPressed = ae.getSource();
+        //Mostrar las demas vistas si se ah precionado el boton respectivo, con el setVisible(true)
         if (buttonPressed == vistaPrincipal.btnAcercaDe) {
             vistaAcercaDe.setVisible(true);
         } else if (buttonPressed == vistaPrincipal.btnCrearVector) {
@@ -73,24 +75,27 @@ public class Controlador implements ActionListener {
             vistaInstructivo.setVisible(true);
         } else if (buttonPressed == vistaPrincipal.btnMostrarListado) {
             vistaMostrarListado.setVisible(true);
+            //poner los datos en la lista
             DefaultListModel<String> modeloLista = new DefaultListModel<>();
-            for (int i = 0; i < modeloMain.getArrayNumbers().length; i++) {
-                modeloLista.addElement(i + ": " + modeloMain.getData(i));
+            for (int i = 0; i < modeloMain.getArrayNumbers().length; i++) {//for para r colocando los datos
+                modeloLista.addElement(i + ": " + modeloMain.getData(i));//mostrar los datos
             }
             vistaMostrarListado.lstPanelListado.setModel(modeloLista);
         } else if (buttonPressed == vistaPrincipal.btnMostrarMayor) {
             vistaMostrarMayor.setVisible(true);
-            int mayor = modeloMain.getData(0);
-            int posicion = 0;
+            int mayor = modeloMain.getData(0);//definimos datos para encontrar el mayor
+            int posicion = 0;//definimos dato para la pocicion
 
-            for (int i = 1; i < modeloMain.getArrayNumbers().length; i++) {
+            for (int i = 1; i < modeloMain.getArrayNumbers().length; i++) {//metodo for para poder encontrar el numero mayor
                 if (modeloMain.getData(i) > mayor) {
-                    mayor = modeloMain.getData(i);
-                    posicion = i;
+                    mayor = modeloMain.getData(i);//cambio por si el numero anterior era inferior al de la pocicion actual
+                    posicion = i;//pocicion del numero mayor
                 }
             }
-            vistaMostrarMayor.lblNumeroMayor.setText(Integer.toString(mayor));
-            vistaMostrarMayor.lblPosicionNumeroMayor.setText(Integer.toString(posicion));
+            vistaMostrarMayor.lblNumeroMayor.setText(Integer.toString(mayor));//mostrar en vista
+            vistaMostrarMayor.lblPosicionNumeroMayor.setText(Integer.toString(posicion));//mostrar en vista
+            
+            //cerrar las vistas
         } else if (buttonPressed == vistaAcercaDe.btnCerrarAcercaDe) {
             vistaAcercaDe.setVisible(false);
         } else if (buttonPressed == vistaCrearVector.btnCerrarCrearVector) {
@@ -104,21 +109,23 @@ public class Controlador implements ActionListener {
         } else if (buttonPressed == vistaMostrarMayor.btnCerrarMostrarMayor) {
             vistaMostrarMayor.setVisible(false);
         } else if (buttonPressed == vistaPrincipal.btnSalir) {
-            vistaPrincipal.setVisible(false);
+            vistaPrincipal.setVisible(false);//cerrar completamente el programa
             vistaPrincipal.dispose();
+            //crear el vector
         } else if (buttonPressed == vistaCrearVector.btnEnviarVector) {
-            modeloMain.setLenghtArray(Integer.parseInt(vistaCrearVector.txtEnviarVector.getText()));
-            modeloMain.createArray();
-            vistaCrearVector.lblCrearVector.setText("Vector Creado. Tamaño: " + modeloMain.getLenghtArray());
-            vistaPrincipal.btnIngresarDatos.setEnabled(true);
-            vistaPrincipal.btnMostrarMayor.setEnabled(true);
-            vistaPrincipal.btnMostrarListado.setEnabled(true);
+            modeloMain.setLenghtArray(Integer.parseInt(vistaCrearVector.txtEnviarVector.getText()));//definir el largo del vector
+            modeloMain.createArray();//crear el vector
+            vistaCrearVector.lblCrearVector.setText("Vector Creado. Tamaño: " + modeloMain.getLenghtArray());//mostrar en vista
+            vistaPrincipal.btnIngresarDatos.setEnabled(true);//habilitar las otras vistas
+            vistaPrincipal.btnMostrarMayor.setEnabled(true);//habilitar las otras vistas
+            vistaPrincipal.btnMostrarListado.setEnabled(true);//habilitar las otras vistas
+            //Recibir los datos y cambiarlos en el vector
         } else if (buttonPressed == vistaIngresarDatos.btnEnviarDatosVector) {
-            int posData = Integer.parseInt(vistaIngresarDatos.txtIngresarPosicion.getText());
-            int valData = Integer.parseInt(vistaIngresarDatos.txtIngresarVariable.getText());
-            vistaIngresarDatos.lblPosicionVector.setText("" + posData);
-            vistaIngresarDatos.lblVariableIngreso.setText("" + valData);
-            modeloMain.loadData(posData, valData);
+            int posData = Integer.parseInt(vistaIngresarDatos.txtIngresarPosicion.getText());//obtenemos dato pocicion
+            int valData = Integer.parseInt(vistaIngresarDatos.txtIngresarVariable.getText());//obtenemos dato "variable"
+            vistaIngresarDatos.lblPosicionVector.setText("" + posData);//mostramos el dato
+            vistaIngresarDatos.lblVariableIngreso.setText("" + valData);//mostramos el dato
+            modeloMain.loadData(posData, valData);//mandamos los datos al modelo "LoadData"
         }
     }
 }
--- src/proyecto/Modelo.java
+++ src/proyecto/Modelo.java
@@ -7,34 +7,34 @@ package proyecto;
 
 public class Modelo {
 
-    int arrayNumbers[];
-    int lenghtArray;
+    int arrayNumbers[];//se da el primer paso para crear el vector
+    int lenghtArray;//se crea el largo delvector
 
-    public int getLenghtArray() {
-        return lenghtArray;
+    public int getLenghtArray() {//metodo para obtener el largo del vector
+        return lenghtArray;//devolver el largo
     }
 
-    public void setLenghtArray(int lenghtArray) {
-        this.lenghtArray = lenghtArray;
+    public void setLenghtArray(int lenghtArray) {//metodo para crear el largo del vector
+        this.lenghtArray = lenghtArray;//sacamos el largo
     }
 
-    public int[] getArrayNumbers() {
-        return arrayNumbers;
+    public int[] getArrayNumbers() {//metodo para obtener los numeros del array
+        return arrayNumbers;//obtenemos los numeros
     }
 
-    public void setArrayNumbers(int[] arrayNumbers) {
-        this.arrayNumbers = arrayNumbers;
+    public void setArrayNumbers(int[] arrayNumbers) {//metodo para colocar un numero en el vector o array
+        this.arrayNumbers = arrayNumbers;//igualamos
     }
 
-    public void createArray() {
-        arrayNumbers = new int[lenghtArray];
+    public void createArray() {//metodo para crear el vector
+        arrayNumbers = new int[lenghtArray];//definimos al vector
     }
 
-    public void loadData(int pos, int data) {
-        arrayNumbers[pos] = data;
+    public void loadData(int pos, int data) {//cargar el dato en la pocicion que queremos
+        arrayNumbers[pos] = data;//se carga el dato
     }
 
-    public int getData(int pos) {
-        return arrayNumbers[pos];
+    public int getData(int pos) {//conseguir la informacion de la pocicion dada
+        return arrayNumbers[pos];//obtenemos
     }
 }
